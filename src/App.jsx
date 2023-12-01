658e81d1f9a01829c20dd1636f8529b2f56d3d92
dadan nurmaulana <dadannurmaulana@gmail.com>
2023-12-01 07:26:51 +0700

    install icon dan belajar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
+import {
+  IconBrandFacebook,
+  IconBrandGithub,
+  IconBrandX,
+} from "@tabler/icons-react";
+import clsx from "clsx";
 import React from "react";
 
-export default function App() {
+const App = () => {
   return (
-    <div className="bg-blue-600 grid place-content-center min-h-screen">
-      <h1 className="text-xl">Learn React JS</h1>
+    <div className="min-h-screen grid bg-slate-900 place-content-center">
+      <div className="flex gap-x-2">
+        <Button
+          {...{
+            type: "submit",
+            onClick: () =>
+              console.log("Hello React"),
+          }}
+        >
+          <IconBrandFacebook />
+          Login
+        </Button>
+        <Button type="submit">
+          <IconBrandX />
+          Login
+        </Button>
+        <Button
+          type="submit"
+          className="bg-black"
+        >
+          <IconBrandGithub />
+          Login
+        </Button>
+      </div>
     </div>
+  );
+};
+
+export default App;
+
+function Button(props) {
+  const {
+    className = "bg-blue-600",
+    children,
+    text,
+  } = props;
+  return (
+    <button
+      {...props}
+      className={clsx(
+        className,
+        "[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex gap-x-2 items-center  text-white px-4 py-2 rounded"
+      )}
+    >
+      {text || children}
+    </button>
   );
 }
