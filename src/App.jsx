d1d923c395d8222a03ab6b957b644a299c6ef21d
dadan nurmaulana <dadannurmaulana@gmail.com>
2023-12-06 06:52:24 +0700

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
-import {
-  IconBrandFacebook,
-  IconBrandGithub,
-  IconBrandX,
-} from "@tabler/icons-react";
-import React from "react";
+import React, { useState } from "react";
+import PlaceContentCenter from "./assets/components/PlaceContentCenter";
 import Button from "./assets/components/Button";
-import Card from "./assets/components/Card";
 
 const App = () => {
+  const [name, setName] = useState("Dadan Nurmaulana");
+  function handleClick() {
+    setName("Idris Maulana");
+  }
   return (
-    <div className="bg-slate-100 antialiased flex items-center justify-center min-h-screen">
-      <div className="max-w-md w-full">
-        <Card>
-          <Card.Title>Hello World</Card.Title>
-          <Card.Body>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
-            fugiat expedita ab, aliquid perspiciatis saepe accusantium cumque
-            laboriosam optio aperiam quia aliquam aspernatur?
-          </Card.Body>
-          <Card.Footer>
-            <Button>Register</Button>
-          </Card.Footer>
-        </Card>
-      </div>
-    </div>
+    <PlaceContentCenter>
+      <div>{name}</div>
+      <Button onClick={handleClick}>Change Name</Button>
+    </PlaceContentCenter>
   );
 };
 
